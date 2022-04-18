5cbbc8500ddda6fd24dd06ba079886978ddf2c8f
UnaiH <71124187+UnaiH@users.noreply.github.com>
2022-04-18 22:39:06 +0200

    FCM hecho y funcional

--- app/src/main/java/com/example/proyectoindividual1/mensajeerror.java
+++ app/src/main/java/com/example/proyectoindividual1/mensajeerror.java
@@ -4,10 +4,19 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.work.Data;
+import androidx.work.OneTimeWorkRequest;
+import androidx.work.WorkInfo;
+import androidx.work.WorkManager;
+
 //Esta clase gestiona la interfaz que permite al usuario identificado comunicar un error.
-public class mensajeerror extends AppCompatActivity {
+public class mensajeerror extends AppCompatActivity implements View.OnClickListener {
     private String usuario;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,5 +64,35 @@ public class mensajeerror extends AppCompatActivity {
     protected void onRestoreInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
         outState.putString("usuario",usuario);
+    }
+    @Override
+    public void onClick(View view) {
+        String aEnviar="Enviado";
+        Log.i("Mensaje", "onClick: "+aEnviar);
+        EditText mens = (EditText) findViewById(R.id.escritomens);
+        aEnviar=""+mens.getText().toString();
+        Log.i("Mensaje", "onClick: "+aEnviar);
+        mens.setText("");
+        Data.Builder data = new Data.Builder();
+        //Se introducen los datos necesarios a pasar a ConexionPHP
+        data.putString("mens",aEnviar);
+        Log.i("Datos", "onClick: "+aEnviar);
+        OneTimeWorkRequest otwr = new OneTimeWorkRequest.Builder(mensajeFCMPHP.class)
+                .setInputData(data.build())
+                .build();
+        WorkManager.getInstance(this).getWorkInfoByIdLiveData(otwr.getId())
+                .observe(this, new Observer<WorkInfo>() {
+                    @Override
+                    public void onChanged(WorkInfo workInfo)
+                    {
+                        //Si se puede iniciar sesión porque devulve true se cambiará la actividad cerrando en la que se encuentra. Si la devolución es null o no es true se mostrará un toast en la interfaz actual.
+                        if(workInfo != null && workInfo.getState().isFinished())
+                        {
+                            String inicio = workInfo.getOutputData().getString("result");
+                            Log.i("TAG", "onChanged: "+inicio);
+                        }
+                    }
+                });
+        WorkManager.getInstance(this).enqueue(otwr);
     }
 }
